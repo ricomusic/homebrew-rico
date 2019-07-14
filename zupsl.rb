08c890b1fcbe9e634718cffe51dfd721110637eb
Ricardo Gonzalez <ricardo76@me.com>
2019-07-14 19:28:15 +0200

    Add ZupSL formula

---
+++ zupsl.rb
@@ -0,0 +1,10 @@
+class Zupsl < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/ricomusic/ZupSL/archive/v1.0.0.tar.gz"
+  sha256 "bbd816949041845e7c6e94a1810aa868497cfbf912c633667429e7aa663dcef4"
+
+  def install
+	bin.install "zupsl"
+  end
+end
