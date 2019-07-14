77b6cc8bb9731525d467cbe8f1ee641d518709cb
Ricardo Gonzalez <ricardo76@me.com>
2019-07-14 19:15:50 +0200

    Add dRails formula

---
+++ drails.rb
@@ -0,0 +1,11 @@
+class Drails < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/ricomusic/dRails/archive/v1.0.0.tar.gz"
+  sha256 "e47dc81c0b92377bbd0c2dae8e27f79601f546381a150406a6f7896a0fee27af"
+
+  def install
+	bin.install "dRails"
+  end
+
+end
